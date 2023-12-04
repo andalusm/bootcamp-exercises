84c43c8d17c14d9a18c115006683dfd013d1fa9b
andalusm <andalus0220@gmail.com>
2023-12-04 14:24:10 +0200

    Finished exercise 3

--- week-4/API/exercises.js
+++ week-4/API/exercises.js
@@ -1,4 +1,4 @@
-
+//Exercise 1
 
 const getBookInfo = function(isbn){
     const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn
@@ -7,6 +7,36 @@ const getBookInfo = function(isbn){
         url:url,
         success: function (result) {
             console.log(isbn + "-"+ result.items[0].volumeInfo.title)
+        }
+    })
+
+}
+
+//Exercise 2
+
+const getBook = function(queryType, queryValue){
+    const url = "https://www.googleapis.com/books/v1/volumes?q="+queryType+":"+queryValue
+    $.ajax({
+        method: "GET",
+        url:url,
+        success: function (result) {
+            console.log( result.items[0].volumeInfo.title )
+        }
+    })
+
+}
+
+//Exercise 3
+
+const getBooks = function(queryType, queryValue){
+    const url = "https://www.googleapis.com/books/v1/volumes?q="+queryType+":"+queryValue
+    $.ajax({
+        method: "GET",
+        url:url,
+        success: function (result) {
+            for (const r of result.items) {
+                console.log(r.volumeInfo.industryIdentifiers[0].identifier + "-"+ r.volumeInfo.title +" BY "+ r.volumeInfo.authors[0] )
+            }
         }
     })
 
