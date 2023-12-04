52cf2ff40dd24744f61633fef4ea1339596f3052
andalusm <andalus0220@gmail.com>
2023-12-04 15:42:48 +0200

    Finished exercise 5

--- week-4/API/exercises.js
+++ week-4/API/exercises.js
@@ -1,12 +1,12 @@
 //Exercise 1
 
-const getBookInfo = function(isbn){
-    const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn
+const getBookInfo = function (isbn) {
+    const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
     $.ajax({
         method: "GET",
-        url:url,
+        url: url,
         success: function (result) {
-            console.log(isbn + "-"+ result.items[0].volumeInfo.title)
+            console.log(isbn + "-" + result.items[0].volumeInfo.title)
         }
     })
 
@@ -14,13 +14,13 @@ const getBookInfo = function(isbn){
 
 //Exercise 2
 
-const getBook = function(queryType, queryValue){
-    const url = "https://www.googleapis.com/books/v1/volumes?q="+queryType+":"+queryValue
+const getBook = function (queryType, queryValue) {
+    const url = "https://www.googleapis.com/books/v1/volumes?q=" + queryType + ":" + queryValue
     $.ajax({
         method: "GET",
-        url:url,
+        url: url,
         success: function (result) {
-            console.log( result.items[0].volumeInfo.title )
+            console.log(result.items[0].volumeInfo.title)
         }
     })
 
@@ -28,16 +28,27 @@ const getBook = function(queryType, queryValue){
 
 //Exercise 3
 
-const getBooks = function(queryType, queryValue){
-    const url = "https://www.googleapis.com/books/v1/volumes?q="+queryType+":"+queryValue
+const getBooks = function (queryType, queryValue) {
+    const url = "https://www.googleapis.com/books/v1/volumes?q=" + queryType + ":" + queryValue
     $.ajax({
         method: "GET",
-        url:url,
+        url: url,
         success: function (result) {
             for (const r of result.items) {
-                console.log(r.volumeInfo.industryIdentifiers[0].identifier + "-"+ r.volumeInfo.title +" BY "+ r.volumeInfo.authors[0] )
+                console.log(r.volumeInfo.industryIdentifiers[0].identifier + "-" + r.volumeInfo.title + " BY " + r.volumeInfo.authors[0])
             }
         }
     })
-
 }
+// Exercise 4
+
+const API_KEY = "eAWD0NLSSsbDIwfDx1Qv7eClJBfSXskS"
+const searchGif = function () {
+    const text = $("#search").val()
+    $.get("http://api.giphy.com/v1/gifs/search?q=" + text + "&api_key=" + API_KEY + "&limit=1", function (data) { 
+        const src = data.data[0].embed_url
+        $('#result').attr('src',src);
+    });
+}
+
+
